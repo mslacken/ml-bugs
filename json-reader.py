3eb721c7e3ebc73590c2667faad3b84adb0d89f2
Christian Goll <cgoll@suse.de>
2019-06-27 17:23:48 +0200

    added random split

--- json-reader.py
+++ json-reader.py
@@ -6,6 +6,7 @@ import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 import keras.preprocessing.text as kr_txt
+import sklearn.model_selection as sk
 
 
 def plot_sample_length_distribution(sample_texts):
@@ -59,6 +60,8 @@ if __name__ == '__main__':
     bugs_count = [] 
     bugs_duration = []
     bugs_summary = []
+    bugs_id1 = []
+    bugs_id2 = []
 
 
     for key,value in bugs.items():
@@ -67,6 +70,8 @@ if __name__ == '__main__':
         bugs_count.append(value['Count'])
         bugs_duration.append(value['etime'] - value['ctime'])
         bugs_summary.append(value['Summary'])
+        bugs_id1.append(key)
+        bugs_id2.append(key)
 
     #plot_sample_length_distribution(bugs_text)
     #plot_sample_distribution(bugs_count)
@@ -80,8 +85,10 @@ if __name__ == '__main__':
     tok = kr_txt.Tokenizer(num_words=args.mwords)
     tok.fit_on_texts(bugs_text)
     bugs_seq = tok.texts_to_sequences(bugs_text)
-    print("First bug report as sequence: ",bugs_seq[0])
-    back_seq = tok.sequences_to_texts(bugs_seq)
-    print("First bug translated back: ", back_seq[0])
+    #print("First bug report as sequence: ",bugs_seq[0])
+    #back_seq = tok.sequences_to_texts(bugs_seq)
+    #print("First bug translated back: ", back_seq[0])
+    x1_train, x1_test, x2_train, x2_test = sk.train_test_split(bugs_id1,bugs_id2,test_size=0.2)
+    print(x1_train[3]," :: ",x2_train[3])
 
     #print("Word count: ", tok.word_counts)
